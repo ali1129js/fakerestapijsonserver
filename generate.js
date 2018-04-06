843bf4eba564e0701db0d8ba8c90f16e149946c0
ali1129js <ali1129js@gmail.com>
2018-04-06 21:33:56 +0200

    edited generater

--- generate.js
+++ generate.js
@@ -1,22 +1,48 @@
+/**
+ * @Author: Ali Ismail
+ * @Date:   2018-04-06T11:50:37+02:00
+ * @Last modified by:   Ali Ismail
+ * @Last modified time: 2018-04-06T21:32:39+02:00
+ */
+
+
+
 var faker = require('faker');
 var _ = require("lodash");
 // with ES6
 module.exports = () =>
 ({
-  people : _.times(100, n =>
+  myCard : _.times(200, n =>
     ({
-      id: n,
-      name: faker.name.findName(),
-      country:faker.address.country(),
-      avatar: faker.internet.avatar(),
-      color: faker.internet.color()
+      person : {
+        id: faker.random.uuid(),
+        name:faker.name.findName(),
+        avatar:faker.internet.avatar(),
+        image:faker.random.image(),
+        color: faker.internet.color(),
+        address: {
+          country:faker.address.country(),
+          state: faker.address.state(),
+          city: faker.address.city(),
+          zipcode: faker.address.zipCode(),
+        },
+        posts: {
+          words:faker.lorem.words(),
+          sentence:faker.lorem.sentence(),
+          slug:faker.lorem.slug(),
+          paragraph:faker.lorem.paragraph(),
+          text:faker.lorem.text(),
+          lines:faker.lorem.lines(),
+        }
+      }
+
     })),
-    comments : _.times(100, n =>
+    userCard : _.times(100, n =>
       ({
         id: n,
-        text: faker.lorem.sentences(),
-        time: faker.date.weekday(),
-        
+        contextualCard: faker.helpers.contextualCard(),
+        userCard: faker.helpers.userCard(),
+        color: faker.internet.color()
       }))
 })
 //
